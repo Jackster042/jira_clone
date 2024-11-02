c104ebb912b04ea987a0553abd783353420770f4
Nemanja Stojanovic <feemail042@gmail.com>
2024-11-02 23:50:34 +0100

    shadcn componnets init setup

---
+++ src/features/test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const test = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold capitalize text-white">test 1123</h2>
+    </div>
+  );
+};
+
+export default test;
