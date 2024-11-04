3ce73237947616f2eda21a04ee0e410a2bcf0bfb
Nemanja Stojanovic <feemail042@gmail.com>
2024-11-04 22:13:10 +0100

    layout basics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Select, SelectTrigger, SelectValue } from "@/components/ui/select";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <div className="flex gap-4 flex-wrap">
         <Input />
-        <Select>
-          <SelectTrigger>
-            <SelectValue placeholder="Select a fruit" />
-          </SelectTrigger>
-        </Select>
         <Button variant={"primary"}>Primary</Button>
         <Button variant={"secondary"}>Secondary</Button>
         <Button variant={"destructive"}>Destructive</Button>
@@ -19,6 +14,8 @@ export default function Home() {
         <Button variant={"outline"}>Outline</Button>
         <Button variant={"muted"}>Muted</Button>
         <Button variant={"teritary"}>Teritrary</Button>
+        <Link href="/sign-in">Sign In</Link>
+        <Link href="/sign-up">Sign Up</Link>
       </div>
     </>
   );
