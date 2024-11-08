680c1be063e3415d71b7e2bc78cc4964c137b0fb
Nemanja Stojanovic <feemail042@gmail.com>
2024-11-08 12:44:19 +0100

    use logout mutation setup | invalidate query on logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
 
 export default function Home() {
   const router = useRouter();
+  const { mutate } = useLogout();
   const { data, isLoading } = useCurrent();
   useEffect(() => {
     if (!data && !isLoading) {
@@ -14,8 +16,11 @@ export default function Home() {
   }, [data]);
   return (
     <>
-      <div className="flex items-center justify-center h-screen">
-        <h1 className="text-2xl font-bold">Only visible to Authirozed Users</h1>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-2xl font-bold">
+          Only visible to Authirozed Users
+          <button onClick={() => mutate()}>Logout</button>
+        </h1>
       </div>
     </>
   );
