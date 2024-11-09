1e8be9a9ffc3f67024fbde85895abde91cbdea77
Nemanja Stojanovic <feemail042@gmail.com>
2024-11-09 23:41:38 +0100

    create workspace route -> api

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -1,5 +1,33 @@
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
 
-const app = new Hono();
+import { createWorkspaceSchema } from "../schemas";
+import { sessionMiddleware } from "@/lib/session-middleware";
+import { ID } from "node-appwrite";
+import { DATABASE_ID, WORKSPACES_ID } from "@/config";
+
+const app = new Hono().post(
+  "/",
+  zValidator("json", createWorkspaceSchema),
+  sessionMiddleware,
+  async (c) => {
+    // CREATE WORKSPACE
+    const databases = c.get("databases");
+    const user = c.get("user");
+
+    const { name } = c.req.valid("json");
+
+    // CREATE DOCUMENT
+    const workspace = await databases.createDocument(
+      DATABASE_ID,
+      WORKSPACES_ID,
+      ID.unique(),
+      {
+        name,
+      }
+    );
+    return c.json({ data: workspace });
+  }
+);
 
 export default app;
