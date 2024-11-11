5fc717f50dc4a3af7a56aeb1cb9eae4674946d29
Nemanja Stojanovic <feemail042@gmail.com>
2024-11-11 21:49:04 +0100

    created imageUrl | node-appwrite server sdk

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -4,7 +4,7 @@ import { zValidator } from "@hono/zod-validator";
 import { createWorkspaceSchema } from "../schemas";
 import { sessionMiddleware } from "@/lib/session-middleware";
 import { ID } from "node-appwrite";
-import { DATABASE_ID, WORKSPACES_ID } from "@/config";
+import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
 
 const app = new Hono().post(
   "/",
@@ -13,9 +13,29 @@ const app = new Hono().post(
   async (c) => {
     // CREATE WORKSPACE
     const databases = c.get("databases");
+    const storage = c.get("storage");
     const user = c.get("user");
 
     const { name, image } = c.req.valid("json");
+
+    let uploadedImageUrl: string | undefined;
+
+    if (image instanceof File) {
+      const file = await storage.createFile(
+        IMAGES_BUCKET_ID,
+        ID.unique(),
+        image
+      );
+
+      //  WE NEED TO CREATE ARRAY BUFFER AND TRANSFORM IT TO BASE64
+      const arrayBuffer = await storage.getFilePreview(
+        IMAGES_BUCKET_ID,
+        file.$id
+      );
+      uploadedImageUrl = `data:image/png;base64,${Buffer.from(
+        arrayBuffer
+      ).toString("base64")}`;
+    }
 
     // CREATE DOCUMENT
     const workspace = await databases.createDocument(
@@ -25,7 +45,7 @@ const app = new Hono().post(
       {
         name,
         userId: user.$id,
-        image,
+        imageUrl: uploadedImageUrl,
       }
     );
     return c.json({ data: workspace });
